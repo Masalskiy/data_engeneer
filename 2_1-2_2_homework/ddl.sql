a0591473337f4f83168d77bfe51672723b55a0cd
Denis Masalskii <masdv973@gmail.com>
2023-10-02 00:27:09 +0300

    modified ddl sql script, add er-diagramm

--- 2_1-2_2_homework/ddl.sql
+++ 2_1-2_2_homework/ddl.sql
@@ -1,21 +1,21 @@
 DROP table public.rchild ; --1
 DROP table public.radult;--2
-drop table public.libcard CASCADE;--3
+drop table public.rlibcard CASCADE;--3
 drop table public.sauthor CASCADE;--4
 drop table public.spublisher CASCADE;--5
 drop table public.sudc CASCADE;--6
-drop table public.sformat CASCADE;--7
-drop table public.sbinding CASCADE;--8
-drop table public.sgenre CASCADE;--9
-drop table public.book CASCADE;--10
+drop table public.cformat CASCADE;--7
+drop table public.cbinding CASCADE;--8
+drop table public.cgenre CASCADE;--9
+drop table public.rbook CASCADE;--10
 drop table public.authorbook;--11
 drop table public.genrebook;--12
 drop table public.instancebook CASCADE;--13
-drop table public.rlibticket cascade;--14
+drop table public.jlibticket cascade;--14
 
 --определить on delete
 
-CREATE TABLE IF NOT EXISTS public.libcard (
+CREATE TABLE IF NOT EXISTS public.rlibcard (
 	pkcard serial not null primary key,
 	lclastn varchar(40) not null,
 	lcfirstn varchar(40) not null,
@@ -23,7 +23,7 @@ CREATE TABLE IF NOT EXISTS public.libcard (
 );--1.1
 
 CREATE TABLE IF NOT EXISTS public.radult (
-	pkacard integer NOT null primary key references libcard ,
+	pkacard integer NOT null primary key references rlibcard ,
 	aaddress varchar(40) not null,
 	aphone varchar(11) not null,
 	apassseriesnum varchar(10) not null,
@@ -31,7 +31,7 @@ CREATE TABLE IF NOT EXISTS public.radult (
 );--1.2
 
 CREATE TABLE IF NOT EXISTS public.rchild (
-	pkccard integer NOT null primary key references libcard,
+	pkccard integer NOT null primary key references rlibcard,
 	fkacard integer not null references radult, 
 	bcert varchar(7) null, -- CHECK(), --должен содержать только цифры
 	releasecrt date NULL
@@ -39,8 +39,9 @@ CREATE TABLE IF NOT EXISTS public.rchild (
 
 CREATE TABLE IF NOT EXISTS public.sauthor (
 	pkidauthor serial NOT null PRIMARY key,
-	alastname varchar(20) NOT null,
-	afirstname varchar(20) not null,
+	alastn varchar(20) NOT null,
+	afirstn varchar(20) not null,
+	athirdn varchar(20) null,
 	acountry varchar(20) not null
 ); --1.4
 
@@ -59,53 +60,53 @@ CREATE TABLE IF NOT EXISTS public.sudc (
 	udesc varchar(20) null
 ); --1.6
 
-create table if not exists public.sformat (
+create table if not exists public.cformat (
 	pkidformat smallserial primary key NOT NULL,
 	fname varchar(20) not null
 ); --1.7
 
-create table if not exists public.sbinding (
+create table if not exists public.cbinding (
 	pkidbinding smallserial primary key NOT null,
 	bname varchar(20) NOT null
 ); --1.8
 
-create table if not exists public.sgenre (
+create table if not exists public.cgenre (
 	idgenres smallserial primary key NOT null,
 	gname varchar(20) NOT null
 ); --1.9
 
-CREATE TABLE IF NOT EXISTS public.book (
+CREATE TABLE IF NOT EXISTS public.rbook (
 	pkisbn varchar(20) primary key NOT null,
 	bname varchar(40) not NULL,
-	bypublic numeric(4) NULL,
-	bquantity integer not null,
-	fkbibpublisher smallint null references spublisher,
+	bypubl numeric(4) NULL,
+	bquantity smallint not null,
+	fkcodepubl smallint null references spublisher,
 	fkbudc varchar(20) not null references sudc,
-	fkbidbformat smallint not null references sformat,
-	fkbidbinding smallint NOT null references sbinding
+	fkbidbformat smallint not null references cformat,
+	fkbidbinding smallint NOT null references cbinding
 );--1.10
 
 CREATE TABLE IF NOT EXISTS public.authorbook(
-	pkfkabisbn varchar(20) not null references book,
-	pkfkab integer  not null references sauthor,
-	primary key (pkfkabisbn, pkfkab)
+	pkfkabisbn varchar(20) not null references rbook,
+	pkfkauthor integer  not null references sauthor,
+	primary key (pkfkabisbn, pkfkauthor)
 );--1.11
 
 CREATE TABLE IF NOT EXISTS public.genrebook (
-	fkgbisbn varchar(20) not null references book,
-	fkgbidgenre smallint not null references sgenre,
+	fkgbisbn varchar(20) not null references rbook,
+	fkgbidgenre smallint not null references cgenre,
 	primary key (fkgbisbn, fkgbidgenre)
 );--1.12
 
 CREATE TABLE IF NOT EXISTS public.instancebook (
 	pkibarcode varchar(20) not null primary key,
-	iisbn varchar(20) not null references book
+	iisbn varchar(20) not null references rbook
 );--1.13
 
-CREATE TABLE IF NOT EXISTS public.rlibticket (
+CREATE TABLE IF NOT EXISTS public.jlibticket (
 -- несколько раз взять одну книгу - одна запись
 -- одна книга в одни руки
-	fktcard INTEGER not null references libcard,
+	fktcard INTEGER not null references rlibcard,
 	fktbarcode varchar(20) not null references instancebook,
 	dategive date not null,
 	datereturnexpect date not null,
